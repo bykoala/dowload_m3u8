660f947cd0dc8df8183245711018251b76298a0f
jiang-ting-hua <59878665+jiang-ting-hua@users.noreply.github.com>
2020-02-05 06:52:16 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sync"
+)
+
+var (
+	url       string
+	maxGo     int = 15
+	waitGroup sync.WaitGroup
+)
+
+func init() {
+	flag.StringVar(&url, "u", "", "M3U8 url index file")
+	flag.IntVar(&maxGo, "c", 15, "maximum number of goroutine")
+}
+func main() {
+	flag.Parse()
+	//url := "https://cn4.qxreader.com/hls/20200131/baeee825f6605d5ab28b954f07e24386/1580471232/index.m3u8"
+	//url:="https://www.nmgxwhz.com:65/20200109/hvg9p8nT/index.m3u8"
+	if url == "" {
+		fmt.Println(`The parameter (u) "M3U8 url" must be entered.`)
+		return
+	}
+
+	fmt.Println("Start initialization M3U8 url index.")
+	err := initAll(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Start dowload M3U8 url ts file.")
+	for i := 0; i < maxGo; i++ {
+		waitGroup.Add(1)
+		go dowload()
+	}
+
+	waitGroup.Wait()
+
+	//尝试重新下载失败的
+	tryFailed()
+
+	//合并文件
+	fmt.Println("Start Merge  ts file, Please wait.")
+	err = tsMerge()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Merge  ts file finish, Download completed.")
+}
